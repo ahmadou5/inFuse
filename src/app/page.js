7923cda0675e9fbdef0bfca0ba492cade72ebdd8
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-19 13:52:38 +0100

    i

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black/45 items-center justify-between ">
+    <main className="flex min-h-screen flex-col bg-black/40 items-center justify-between ">
       <Home2 />
      
     </main>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from "react";
-import { IoSettings } from "react-icons/io5";
+import { IoArrowDown, IoArrowUp, IoScan, IoSettings } from "react-icons/io5";
 
 export const Home2 = () => {
     const [user,setUser ] = useState(null)
@@ -27,12 +27,33 @@ export const Home2 = () => {
     <div className="w-[100%] py-2 px-1 h-auto bg-red-400/0">
         <div className="w-[100%] h-12 flex  py-3 ">
             <div className="w-[85%]">
-            <div className="w-[145px]  ml-auto mr-[63px] py-1 px-3 flex  items-center justify-center bg-s-gray-200/85 rounded-full h-8">
-                <p className="text-black font-light ml-auto mr-auto ">{user?.initDataUnsafe?.user?.username}</p>
+            <div className="w-[145px]  ml-auto mr-[63px] py-1 px-3 flex  items-center justify-center bg-gothic-600 rounded-full h-9">
+                <p className="text-white font-light ml-auto mr-auto ">{user?.initDataUnsafe?.user?.username}</p>
             </div>
             </div>
             <div className="w-[15%]">
-             <IoSettings className="w-7 mt-0.5 h-7 ml-auto mr-5 text-s-gray-200/70"/>
+             <IoSettings className="w-7 mt-0.5 h-7 ml-auto mr-5 text-gothic-600/85"/>
+            </div>
+        </div>
+        <div className="bg-gothic-950/0 mt-12 flex items-center justify-center w-[100%] h-auto">
+            <div className="bg-s-gray-300/0 w-[90%] flex items-center justify-center rounded-3xl h-[140px]">
+                <p className="text-6xl  text-gothic-200/85">{`$${50}`}</p>
+            </div>
+        </div>
+        <div className="bg-gothic-950/0 mt-7 flex items-center justify-center w-[100%] h-auto">
+            <div className="bg-gothic-300/5 w-[90%] flex items-center justify-center rounded-3xl h-[100px]">
+                <div className="text-xl bg-gothic-600/0 flex flex-col items-center justify-center rounded-3xl h-20 w-20 ml-auto mr-auto  text-white/60">
+                    <IoArrowUp className="text-3xl text-gothic-600/85" />
+                  <p className="text-xl font-light ">Send</p>
+                </div>
+                <div className="text-3xl bg-gothic-600/0 flex flex-col items-center justify-center rounded-3xl h-20 w-20 ml-auto mr-auto  text-white/60">
+                    <IoArrowDown className="text-3xl text-gothic-600/85" />
+                  <p className="text-xl font-light ">Receive</p>
+                </div>
+                <div className="text-xl bg-gothic-600/0 flex flex-col items-center justify-center rounded-3xl h-20 w-20 ml-auto mr-auto  text-white/60">
+                    <IoScan className="text-3xl text-gothic-600/85"/>
+                  <p className="text-xl font-light ">Scan</p>
+                </div>
             </div>
         </div>
     </div>

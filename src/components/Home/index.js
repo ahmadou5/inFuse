7714c899103d5872110b6a239b99056dfbf67e30
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-19 13:03:27 +0100

    init23478890007hscript7

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,18 +8,27 @@ export const Home2 = () => {
         
     }
     useEffect(() => {
-        if( window.Telegram?.WebApp) {
-            const { initialData } = window.Telegram?.WebApp;
-            alert(initialData.user.username)
-            //setUser(initialData.user)
+        console.log('useTelegram')
+        function initTg() {
+        if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
+        console.log('Telegram WebApp is set');
+        const tgData = window.Telegram.WebApp
+        setUser(tgData);
+        } else {
+        console.log('Telegram WebApp is undefined, retrying…');
+        console.log(user)
+        setTimeout(initTg, 500);
         }
-    },[])
+        }
+        initTg();
+      }, []);
+   
     return(
     <div className="w-[100%] py-2 px-1 h-auto bg-red-400/0">
         <div className="w-[100%] h-12 flex  py-3 ">
             <div className="w-[85%]">
             <div className="w-[145px]  ml-auto mr-[63px] py-1 px-3 flex  items-center justify-center bg-s-gray-200/85 rounded-full h-8">
-                <p className="text-black font-light ml-auto mr-auto ">{'Hello'}</p>
+                <p className="text-black font-light ml-auto mr-auto ">{user?.initDataUnsafe?.user?.id}</p>
             </div>
             </div>
             <div className="w-[15%]">

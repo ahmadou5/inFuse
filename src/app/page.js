593c64b94c90e89cb4b195d96bee3bb7112e427a
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-19 12:00:23 +0100

    init2

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
+import { Home2 } from "@/components/Home";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-s-gray-950 items-center justify-between ">
-      <div className="h-[300px] w-[95%] py-8 px-3 mt-10 rounded-2xl bg-s-gray-200">
-      <p className="text-4xl text-gothic-600">
-        Hello Welcome to InFuse Bot
-      </p>
-      </div>
+    <main className="flex min-h-screen flex-col bg-black/45 items-center justify-between ">
+      <Home2 />
      
     </main>
   );
---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import { IoSettings } from "react-icons/io5";
+
+export const Home2 = () => {
+    return(
+    <div className="w-[100%] py-2 px-1 h-auto bg-red-400/0">
+        <div className="w-[100%] h-12 flex  py-3 ">
+            <div className="w-[85%]">
+            <div className="w-[145px]  ml-auto mr-[63px] py-1 px-3 flex  items-center justify-center bg-s-gray-200 rounded-full h-8">
+                <p className="text-black font-light ml-auto mr-auto ">Wallet</p>
+            </div>
+            </div>
+            <div className="w-[15%]">
+             <IoSettings className="w-8 h-8 ml-auto mr-5 text-s-gray-200"/>
+            </div>
+        </div>
+    </div>
+)
+}

69304afe75bbada790fa7f5c0c017046a1059dc6
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-20 11:28:10 +0100

    create66

--- src/components/Home/Create.js
+++ src/components/Home/Create.js
@@ -26,18 +26,18 @@ export const Create = () => {
    
     return(
     <div className="w-[100%] py-2 px-1 h-auto bg-red-400/0">
-        <div className="bg-gothic-950/0 mt-[100px] mb-[60px] flex items-center justify-center w-[100%] h-auto">
-            <img src="./assets/chain1.svg" className="w-[55%] h-[210px]" />
+        <div className="bg-gothic-950/0 mt-[170px] mb-[40px] flex items-center justify-center w-[100%] h-auto">
+            <img src="./assets/chain1.svg" className="w-[40%] h-[150px]" />
         </div>
         <div className="bg-gothic-950/0 mt-3 mb-8 flex items-center justify-center w-[100%] h-auto">
             <div className="bg-s-gray-300/0 w-[90%] px-10 flex flex-col items-center justify-center rounded-3xl h-[140px]">
-                <p className="text-4xl font-extrabold mb-6 text-gothic-200/85">{`Infuse Wallet`}</p>
-                <p className="text-xl font-extrabold text-center mt-4 text-gothic-200/85">{`Hi ${user?.initDataUnsafe?.user?.username} Create a new wallet or import an existing one`}</p>
+                <p className="text-3xl font-extrabold mb-6 text-gothic-200/85">{`Infuse Wallet`}</p>
+                <p className="text-sm font-extrabold text-center mt-4 text-gothic-200/85">{`Hi ${user?.initDataUnsafe?.user?.username} Create a new wallet or import an existing one`}</p>
             </div>
         </div>
-        <div className="bg-s-gray-300/0 w-[95%] ml-auto mr-auto mt-16 px-2 flex flex-col items-center justify-center rounded-2xl h-auto">
-                <button onClick={() => set} className="text-xl bg-s-gray-900/75 w-[240px] mb-3 h-11 rounded-3xl font-extrabold ">{`Create `}</button>
-                <button className="text-xl bg-gothic-200 w-[240px] text-s-gray-700 mt-3 h-11 rounded-3xl font-extrabold ">{`Import`}</button>
+        <div className="bg-s-gray-300/0 w-[95%] ml-auto mr-auto mt-12 px-2 flex flex-col items-center justify-center rounded-2xl h-auto">
+                <button onClick={() => set} className="text-xl bg-s-gray-900/75 w-[290px] mb-3 h-12 rounded-xl font-extrabold ">{`Create `}</button>
+                <button className="text-xl bg-gothic-200 w-[290px] text-s-gray-700 mt-3 h-12 rounded-xl font-extrabold ">{`Import`}</button>
         </div>
     </div>
 )

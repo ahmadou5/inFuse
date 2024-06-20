32b95594acc058a25fabeac5f77a14faf1069e5b
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-20 01:34:35 +0100

    20a6890

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -36,7 +36,7 @@ export const Home2 = () => {
              <IoSettings className="w-7 mt-0.5 h-7 ml-auto mr-5 text-gothic-600/85"/>
             </div>
         </div>
-        <div className="bg-gothic-950/0 mt-8 flex items-center justify-center w-[100%] h-auto">
+        <div className="bg-gothic-950/0 mt-5 flex items-center justify-center w-[100%] h-auto">
             <div className="bg-s-gray-300/0 w-[90%] flex items-center justify-center rounded-3xl h-[140px]">
                 <p className="text-4xl  text-gothic-200/85">{`$${0}`}</p>
             </div>
@@ -67,7 +67,7 @@ export const Home2 = () => {
                 </div>
             </div>
     </div> **/}
-        <div className="bg-gothic-950/0 mt-5 flex items-center justify-center w-[100%] h-auto">
+        <div className="bg-gothic-950/0 mt-12 flex items-center justify-center w-[100%] h-auto">
             <div className="bg-gothic-300/5 w-[90%] flex items-center justify-center rounded-3xl h-[100px]">
                 <div className="bg-gothic-600/85 w-12 flex items-center justify-center h-12 ml-[23px] mr-[10px] rounded-full">
                     <img src="./assets/chain1.svg" className="text-white/90 w-full h-full text-2xl" />
--- src/components/Home/menu.js
+++ src/components/Home/menu.js
@@ -51,7 +51,7 @@ export const Menu = () => {
         {/**for desktop view **/}
         <div
           style={{ "backdrop-filter": "blur(12px)" }}
-          className=" w-[90%] ml-auto mr-auto rounded-full bg-s-gray-300/0 py-1 px-1.5 z-100 mt-[550px]  fixed inset-x-0 top-2 flex justify-center items-center"
+          className=" w-[97%] ml-auto mr-auto rounded-full bg-s-gray-300/10 py-1 px-1.5 z-100 mt-[560px]  fixed inset-x-0 top-2 flex justify-center items-center"
         >
           <div className="lg:py-2.5 py-1.5 lg:px-2.5 px-1.5  mt-auto mb-auto ml-auto mr-auto w-[98%] flex flex-row  h-[90%]">
             <div className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>

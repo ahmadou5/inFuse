8b200191eaca16cb75a68d8b8e8034e3b0fe7ed8
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-20 01:47:51 +0100

    ib467tlu

--- src/components/Home/menu.js
+++ src/components/Home/menu.js
@@ -56,15 +56,28 @@ export const Menu = () => {
           className=" w-[93%] ml-auto mr-auto rounded-full py-1 px-1.5 z-100 mt-[560px]  fixed inset-x-0 top-2 flex justify-center items-center"
         >
           <div className="lg:py-2.5 py-1.5 lg:px-2.5 px-1.5  mt-auto mb-auto ml-auto mr-auto w-[98%] flex flex-row  h-[90%]">
-            <div className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>
+            <div onClick={() => {
+              setIsHistory(false)
+              setIsTokens(false)
+              setIsHome(true)
+            }} className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>
                 <IoWallet size={28} className={`${ isHome ? 'text-white' : 'text-gothic-600/85'}`} />
                 {isHome && <p className={`font-light ${isHome ? 'text-white' : 'text-gothic-200'} text-[15px]`}>Wallet</p>}
             </div>
-            <div className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>
+            <div onClick={() => {
+              setIsHistory(true)
+              setIsTokens(false)
+              setIsHome(false)
+            }}
+             className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>
                 <IoFlash size={28} className={`${ isHistory ? 'text-white' : 'text-gothic-600/85'}`} />
                 {isHistory && <p className={`font-light ${isHistory ? 'text-white' : 'text-gothic-200'} text-[15px]`}>Transaction</p>}
             </div>
-            <div className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>
+            <div onClick={() => {
+              setIsHistory(false)
+              setIsTokens(true)
+              setIsHome(false)
+            }} className={`h-12 ml-auto mr-auto w-[30%] bg-white/0 flex flex-col items-center justify-center`}>
                 <BsCoin size={28} className={`${ isTokens ? 'text-white' : 'text-gothic-600/85'}`} />
                 {isTokens && <p className={`font-light ${isTokens ? 'text-white' : 'text-gothic-200'} text-[15px]`}>Tokens</p>}
             </div>

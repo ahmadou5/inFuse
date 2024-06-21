594250eee2a47d99014c3b27c3f466a4a62cf653
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-21 17:44:31 +0100

    creat66555787889

--- src/hooks/useGetUserId.js
+++ src/hooks/useGetUserId.js
@@ -21,8 +21,8 @@ export const useGetUserId = () => {
                     setIsAuthenticate(true)
                 }
             }
+            check()
          }
-         check()
     },[user])
     return true
 }

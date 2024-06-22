51db3eda54de7ad44dfad85391f13c56160b8121
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-22 12:51:00 +0100

    chore:almostdone493eaa3ttjtdgfgsfhsfgjjj

--- src/components/Modals/WelcomeModal.js
+++ src/components/Modals/WelcomeModal.js
@@ -5,7 +5,7 @@ export const Welcome = () => {
     return(
     <div className="inset-0 fixed bg-black/55 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
         <div className="w-[100%] flex items-center px- justify-center">
-            <div className="h-[170px] ml-auto mr-auto py-4 px-3 w-[90%] bg-white/15 rounded-xl">
+            <div className="h-[170px] ml-auto mr-auto py-4 px-3 w-[90%] bg-black rounded-xl">
             <div>
                 <div onClick={() => setWelcome(false)} className="w-20 rounded-xl text-xl font-light flex items-center justify-center h-9 bg-white/5">
                     <p>esc</p>

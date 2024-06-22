7576e0ee917cf3273eabb6b061beaad0977b1320
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-22 20:37:11 +0100

    chore:newVVu0

--- src/components/Modals/WelcomeModal.js
+++ src/components/Modals/WelcomeModal.js
@@ -3,7 +3,7 @@ import { GlobalContext } from "@/Context/AppContext"
 export const Welcome = () => {
     const { setWelcome, userName } = GlobalContext()
     return(
-    <div className="inset-0 fixed bg-black/45 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
+    <div className="inset-0 fixed bg-white bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
         <div className="w-[100%] flex items-center px- justify-center">
             <div className="h-[190px] ml-auto mr-auto py-2 px-2 w-[90%] bg-black rounded-xl">
             <div>

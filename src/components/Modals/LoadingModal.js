85796e0d082fd5f3dcd14b8a0a51f56488e66693
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-22 12:53:20 +0100

    chore:almostd

--- src/components/Modals/LoadingModal.js
+++ src/components/Modals/LoadingModal.js
@@ -1,8 +1,10 @@
 export const Loading = () => {
     return(
     <div className="inset-0 fixed bg-black/95 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
-        <div className="w-[100%] flex flex-col items-center px-6 justify-center">
-            <p className="">Loading...</p>
+        <div className="w-[100%] h-[auto] flex items-center justify-center">
+           <div className="w-20 h-20 bg-white rounded-lg">
+            <p className="text-center text-black">Loading</p>
+           </div>
         </div>
     </div>
     )

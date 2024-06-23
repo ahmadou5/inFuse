a0a7e09a2cd4e19957924711730878edee3137ef
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-23 15:14:38 +0100

    sundayETHBalance12554422wqq76Slice6ggumultiple73w

--- src/components/Modals/SendModal.js
+++ src/components/Modals/SendModal.js
@@ -26,7 +26,7 @@ export const SendModal = () => {
     }
     return(
     <div className="inset-0 fixed bg-black bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
-        <div className="w-[100%] py-4 px-4 bg-[#4f4f4f] border-white rounded-t-3xl h-auto mt-[100px]">
+        <div className="w-[100%] py-4 px-4 bg-white/85 border-white rounded-t-3xl h-auto mt-[100px]">
             <div className="">
                 <div onClick={() => setIsSend(false)} className="w-20 rounded-xl text-xl font-light flex items-center justify-center h-9 bg-white/5">
                     <p>esc</p>

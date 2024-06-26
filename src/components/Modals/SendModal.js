89126cd1396a021b11b160b75c4e20e3c7c23628
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-26 21:18:38 +0100

    errormesdsdsdjyklhhhhshhjjgchjhk

--- src/components/Modals/SendModal.js
+++ src/components/Modals/SendModal.js
@@ -63,7 +63,7 @@ export const SendModal = () => {
                 isConfirmed ? 
             <div className="mt-8 px-2 py-3 bg-red-600/0 h-[85%] flex flex-col rounded-xl w-[99%] ml-auto mr-auto">
                <div className="w-[100%] h-12 bg-slate-50/0 rounded-xl py-3 px-6">
-                <p className="text-[16px] font-light">{`to: ${formatAddress(receiveAddress)}`}</p>
+                <p className="text-[19px] text-black font-light">{`to: ${formatAddress(receiveAddress)}`}</p>
                </div>
                <div className="w-[98%] mt-4 ml-auto mr-auto h-[230px] py-3 px-2 flex flex-col items-center justify-center rounded-2xl bg-black/20">
                 <div className="w-[100%] ml-auto mr-auto text-black rounded-xl  flex  h-16">
@@ -99,8 +99,8 @@ export const SendModal = () => {
             <div className="mt-8 px-2 py-3 bg-red-600/0 h-[85%] flex flex-col rounded-xl w-[99%] ml-auto mr-auto">
             <div className="mt-12 w-[100%] ml-auto mr-auto">
             <div className="w-[100%] ml-auto mr-auto mb-4 flex rounded-xl text-[19px] text-black/75 py-3 px-3 border items-center justify-center border-black bg-black/0 h-16">
-               <p className="font-light">From:</p>
-               <p className="font-light">{formatAddress(userAddress)}</p>
+               <p className=" text-black font-light">From:</p>
+               <p className=" text-black font-light">{formatAddress(userAddress)}</p>
              </div>
              <div className="w-[100%] ml-auto mr-auto rounded-xl text-xl border border-black bg-black/0 h-16">
                  <input onChange={(e) => setReceiveAddress(e.target.value)} type="text" className="outline-none text-[19px] text-black/75 bg-transparent w-[100%] h-[100%]  py-2 px-4" placeholder="Address" />

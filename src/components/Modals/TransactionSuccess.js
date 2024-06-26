02ca457231f199087cf540ddaab997dec0e2ac91
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-26 20:36:51 +0100

    errormesdsdsdjyklhhhhs

--- src/components/Modals/TransactionSuccess.js
+++ src/components/Modals/TransactionSuccess.js
@@ -1,6 +1,7 @@
 import { GlobalContext } from "@/Context/AppContext"
 import { useGetUserId } from "@/hooks/useGetUserId"
 import { formatAddress } from "@/Utils/format"
+import Link from "next/link"
 
 export const TransactionSuccessModal = ({hash,amount}) => {
     const { setWelcome, userName,isTxSuccess,setIsTxSuccess, setIsSend } = GlobalContext()
@@ -15,7 +16,10 @@ export const TransactionSuccessModal = ({hash,amount}) => {
                   <p className="text-black/85 font-light ml-auto mr-auto ">{`You just send ${amount} ETH now`}</p>
                 </div>
                 <div className="w-[100%]  ml-auto mr-auto py-1 px-3 flex  items-center justify-center bg-white/30 rounded-full h-9">
-                  <p className="text-black/85 font-light ml-auto mr-auto ">{`Tx Hash: ${formatAddress(hash)}`}</p>
+                  <p className="text-black/85 font-light ml-auto mr-1 ">{`Tx Hash:`}</p>
+                  <Link href={`https://sepolia.etherscan.io/tx/${hash}`} target="_blank">
+                   <p className="text-black font-light ml-1 mr-auto ">{`${hash}`}</p>
+                  </Link> 
                 </div>
                 <div onClick={() => {
                     setIsSend(false)

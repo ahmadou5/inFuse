f0d03f48ba038b63d96db9eed4a43e8e316dfbe3
ahmadou5 <ahmadlasauwal@gmail.com>
2024-06-29 09:00:47 +0100

    UI33FFyhhhhhhh5568hhhhnnkktxdjj

--- src/hooks/useGetTransaction.js
+++ src/hooks/useGetTransaction.js
@@ -11,14 +11,14 @@ export const useGetTransaction = () => {
         const blockN = await Provider.getBlockNumber()
         const block = await Provider.getBlock(blockN);
         const pastTransactions = await Provider.getLogs({
-            fromBlock: Math.max(0, blockN - 1000), // Check past 100 blocks
+            fromBlock: Math.max(0, blockN - 100), // Check past 100 blocks
             toBlock: blockN,
             address:userAddress,
           });
           const newReceivedTransactions = pastTransactions.filter((tx) => tx.to === userAddress).filter((tx, index, self) => tx.to === userAddress &&
             self.findIndex((t) => t.hash === tx.hash) === index
           );
-        const userTransactions = block.transactions.filter((tx) => tx.to === userAddress)
+        const userTransactions = block.transactions()
         setTrx(newReceivedTransactions)
         console.log('block',blockN)
         console.log('User',userTransactions)

d58db3ddbc90ee7c6fe4de1c514205d75bebfc39
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-10 20:56:56 +0100

    test288ee

--- src/components/Modals/LoadingModal.js
+++ src/components/Modals/LoadingModal.js
@@ -1,12 +1,12 @@
 import { SpinningCircles } from 'react-loading-icons'
 export const Loading = () => {
     return(
-    <div className="inset-0 fixed bg-black bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
-        <div className="w-[100%] h-[auto] flex items-center justify-center">
-           <div className="w-auto h-auto mt-4">
-            <SpinningCircles />
-            <p className="text-center mt-5 text-white">Loading...</p>
+        <div className="inset-0 fixed bg-black/60 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
+        <div className="w-[100%] h-[auto] flex flex-col items-center justify-center">
+           <div className="w-auto h-auto mt-1">
+            <img src='https://console.fuse.io/_next/static/media/fuse-icon.46cbae3d.svg' className="w-[250px] h-[250px]" />
            </div>
+           <div class="loader"></div> 
         </div>
     </div>
     )

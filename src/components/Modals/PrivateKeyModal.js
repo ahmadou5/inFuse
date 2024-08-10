634b149dca8b6124b3c7162d6c8e75d928d82d25
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-10 20:29:00 +0100

    test288ee

--- src/components/Modals/PrivateKeyModal.js
+++ src/components/Modals/PrivateKeyModal.js
@@ -59,7 +59,7 @@ export const PkeyModal = () => {
           </div>
           <div className="w-[98%] mt-12 mb-10 ml-auto mr-auto bg-black/75 h-[290px] py-3 px-1 flex flex-col items-center justify-center rounded-2xl">
           <div className="text-white text-sm font-light">
-           {userPkey}
+           {formatAddress(userPkey)}
           </div>
           
           </div>

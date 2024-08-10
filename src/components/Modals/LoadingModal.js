be70f6e62f5d127c6faff04217700492119de591
ahmadou5 <ahmadlasauwal@gmail.com>
2024-08-10 21:03:55 +0100

    test288ee

--- src/components/Modals/LoadingModal.js
+++ src/components/Modals/LoadingModal.js
@@ -3,7 +3,7 @@ export const Loading = () => {
     return(
         <div className="inset-0 fixed bg-black/60 bg-opacity-100 w-[100%] z-[99999999] min-h-screen h-auto backdrop-blur-sm flex ">
         <div className="w-[100%] h-[auto] flex flex-col items-center justify-center">
-           <div className="w-auto h-auto mt-1">
+           <div className="w-auto h-auto mt-1 mb-4">
             <img src='https://earn-fuse.vercel.app/assets/show.png' className="w-[250px] h-[250px]" />
            </div>
            <div class="loader"></div> 

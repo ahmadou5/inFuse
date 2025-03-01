8d9a2bab9ad5673a2aa609352c5010e4b963122c
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2025-03-01 14:32:50 +0100

    Update format.js

--- src/Utils/format.js
+++ src/Utils/format.js
@@ -25,8 +25,9 @@ export const formatAddress = (value) => {
       }
     );
   }
+ 
   export const chains = [
-    {
+      {
       name: 'Fuse',
       providerUrl: 'https://rpc.fuse.io',
       imgUrl: 'https://console.fuse.io/_next/static/media/fuse-icon.46cbae3d.svg',
@@ -34,8 +35,6 @@ export const formatAddress = (value) => {
       scan: 'https://explorer.fuse.io/',
       faucet: 'https://www.alchemy.com/faucets/ethereum-sepolia'
     },
-  ]
-  export const chain = [
     {
       name: 'Fraxtal',
       providerUrl: 'https://rpc.testnet.frax.com',
